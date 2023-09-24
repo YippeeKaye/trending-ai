ac69294f9d209a29f567c28a547d2cb2be83eb91
Yippee Kaye <baptistabomb86+github@gmail.com>
2023-09-24 22:20:59 +0200

    add prettier rules

--- app/dbFunctions/models.tsx
+++ app/dbFunctions/models.tsx
@@ -1,25 +1,22 @@
-
 import sqlite3 from 'sqlite3';
-
 
 const db = new sqlite3.Database('./mydb.sqlite');
 
-
-export async function listModelsSortedByRuns() {
-    try {
-      const rows = await new Promise((resolve, reject) => {
-        db.all('SELECT * FROM models ORDER BY runs DESC', (err, rows) => {
-          if (err) {
-            console.error(err);
-            reject(err);
-          } else {
-            resolve(rows);
-          }
-        });
+export async function listModelsSortedByRuns () {
+  try {
+    const rows = await new Promise((resolve, reject) => {
+      db.all('SELECT * FROM models ORDER BY runs DESC', (err, rows) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(rows);
+        }
       });
-      return rows;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    });
+    return rows;
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
+}
